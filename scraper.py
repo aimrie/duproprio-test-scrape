32a831f8463e2304697f174571cc199ab6499a4f
Aaron Imrie <aimrie@etcmd.com>
2022-07-05 17:37:06 -0500

    Add some output so you know it's working. Add sleep

--- scraper.py
+++ scraper.py
@@ -2,10 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
+from tqdm import tqdm
+
+# python -m pip install requests beatifulsoup4 pandas tqdm
 
 homes = []
 
-for pageNumber in range(1,5):
+# scraping pages 1 to 5 (non-inclusive)
+for pageNumber in tqdm(range(1,5)):
     url = f'https://duproprio.com/en/search/list?search=true&is_for_sale=1&with_builders=1&parent=1&pageNumber={pageNumber}&sort=-published_at'
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
@@ -31,7 +35,14 @@ for pageNumber in range(1,5):
         phone = soup.find('a', class_="gtm-listing-link-contact-owner-phone")['href']
 
         home['phone'] = phone
+        
+        # Sleeping 1 second between pulls
+        # so that hopefully duproprio doesn't get mad :)
+        time.sleep(1)
+    
+    print(f"Homes processed: {len(homes)}")
 
 df = pd.DataFrame(homes)
+print(df.head())
 
 df.to_csv('homes.csv')
